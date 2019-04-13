0d700b2ab5b62436abf7ad79bf1f2edbfeaf109d
Maginor <magnus.dahler.norling@gmail.com>
2019-04-13 18:31:35 +0200

    Delete compile.bat

--- Applications/TestModel/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-REM C:\Mingw\bin\g++ main.cpp -O2 -fno-exceptions  -ffast-math -Werror=return-type -o simplyp.exe -fmax-errors=5
-g++ main.cpp -std=c++11 -O2 -Werror=return-type -o test.exe -fmax-errors=5
