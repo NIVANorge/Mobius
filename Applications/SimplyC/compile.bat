af9862db1fdd4dd82aac44b202beb1a61caee2b1
Leah Jackson-Blake <leah.jackson-blake@niva.no>
2019-04-26 15:15:11 +0200

    Started tidying SimplyC files and folders

--- Applications/SimplyC/compile.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-g++ SimplyC_test.cpp -O2 -std=c++11 -fno-exceptions -o SimplyC_test.exe -fmax-errors=5
--- PythonWrapper/SimplyC/compile.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-g++ -c -m64 -std=c++11 -O2 SimplyC_wrapper.cpp -fexceptions -fmax-errors=5
-g++ -o simplyc.dll -s -shared simplyc_wrapper.o -Wl,--subsystem,windows
