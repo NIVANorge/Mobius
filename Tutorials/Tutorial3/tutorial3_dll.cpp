be1139a7404ef579ea06f71905b1041b99773961
Maginor84 <magnus.dahler.norling@gmail.com>
2019-06-04 16:27:38 +0200

    Added ability to compile tutorial3 to a .dll

---
+++ Tutorials/Tutorial3/tutorial3_dll.cpp
@@ -0,0 +1,43 @@
+#define MOBIUS_TIMESTEP_VERBOSITY 0
+#define MOBIUS_TEST_FOR_NAN 0
+#define MOBIUS_EQUATION_PROFILING 0
+#define MOBIUS_PRINT_TIMING_INFO 0
+#define MOBIUS_INDEX_BOUNDS_TESTS 0
+
+#include "../../mobius_dll.h"
+
+#include "GibletThrasherModel.h"
+
+
+
+DLLEXPORT void *
+DllSetupModel(char *ParameterFilename, char *InputFilename)
+{
+	//NOTE: This is just an example of creating a model .dll that can be used with the python wrapper or MobiView.
+	
+	CHECK_ERROR_BEGIN
+	
+	mobius_model *Model = BeginModelDefinition("Giblet-thrasher model", "1.0");
+	
+	auto Days 	      = RegisterUnit(Model, "days");
+	auto System       = RegisterParameterGroup(Model, "System");
+	RegisterParameterUInt(Model, System, "Timesteps", Days, 100);
+	RegisterParameterDate(Model, System, "Start date", "1999-1-1");
+	
+	AddGibletThrasherModel(Model);
+	
+	ReadInputDependenciesFromFile(Model, InputFilename);
+	
+	EndModelDefinition(Model);
+	
+	mobius_data_set *DataSet = GenerateDataSet(Model);
+	
+	ReadParametersFromFile(DataSet, ParameterFilename);
+	ReadInputsFromFile(DataSet, InputFilename);
+	
+	return (void *)DataSet;
+	
+	CHECK_ERROR_END
+	
+	return 0;
+}
