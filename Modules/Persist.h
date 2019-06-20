8d1e0ffd8dffce069f37471e945b7337a0f962d6
Maginor84 <magnus.dahler.norling@gmail.com>
2019-06-20 13:46:17 +0200

    Fixed bug with capacity computation for percolation in PERSiST.

--- Modules/Persist.h
+++ Modules/Persist.h
@@ -253,7 +253,10 @@ AddPersistModel(mobius_model *Model)
 		
 		for(index_t OtherSoil = CURRENT_INDEX(SoilBoxes) + 1; OtherSoil < INDEX_COUNT(SoilBoxes); ++OtherSoil)
 		{
-			double mpi = Min(PARAMETER(Infiltration, OtherSoil, LU), (PARAMETER(MaximumCapacity, OtherSoil, LU) - RESULT(WaterDepth3, OtherSoil))); //NOTE: RESULT(WaterDepth3, OtherSoil) has not been computed yet, and so will be 0. In fact, it depends on values computed in this equation (mainly RESULT(PercolationInput, OtherSoil), and so can not possibly be computed before this unless the model structure is rewritten. TODO: Do something else here, maybe use LAST_RESULT(WaterDepth, OtherSoil) or similar.
+			double othersoildepth = LAST_RESULT(WaterDepth, OtherSoil) + RESULT(PercolationInput, OtherSoil);
+			double othersoilremainingcap = PARAMETER(MaximumCapacity, OtherSoil, LU) - othersoildepth;
+			
+			double mpi = Min(PARAMETER(Infiltration, OtherSoil, LU), othersoilremainingcap);
 			
 			double perc = Min(mpi * PARAMETER(RelativeAreaIndex, OtherSoil, LU) / relativeareaindex,
 				PARAMETER(PercolationMatrix, OtherSoil) * totalrunoff);
