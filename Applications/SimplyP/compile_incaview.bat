1802d3464ecb5f2b652532c6e9abdfa4557fc142
Leah Jackson-Blake <leah.jackson-blake@niva.no>
2019-06-26 13:42:26 +0200

    Remove INCAView-related files from SimplyC and SimplyP
    Plus update to SimplyP readme to reflect this.
    Plus minor renaming of some SimplyP files

--- Applications/SimplyP/compile_incaview.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM g++ -O2 -I../../Calibration/MCMC/mcmc/include/ -I../../Calibration/MCMC/mcmc/src/ incaviewsimplyp.cpp -m64 -std=c++11 -fopenmp -o incaviewsimplyp.exe ../../sqlite3/sqlite3.o -L../../Calibration/MCMC/libs/ -llapack_win64_MT -lblas_win64_MT -DINCAVIEW_INCLUDE_OPTIMIZER=1 -DINCAVIEW_INCLUDE_GLUE=1 -DINCAVIEW_INCLUDE_MCMC=1
-g++ simplyp_incaview.cpp -O2 -std=c++11 -o SimplyP_INCAView.exe ../../sqlite3/sqlite3.o -fmax-errors=5
