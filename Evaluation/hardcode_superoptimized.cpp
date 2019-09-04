fa8ebcbf915a3ec51ed0ed61b0eef01e3387cfab
Maginor84 <magnus.dahler.norling@gmail.com>
2019-09-04 12:22:28 +0200

    Some more SIMD testing

--- Evaluation/hardcode_superoptimized.cpp
+++ Evaluation/hardcode_superoptimized.cpp
@@ -36,8 +36,11 @@ int main()
 	
 	u64 Timesteps = GetParameterUInt(DataSet, "Timesteps", {});
 	
-	u64 DataLen = Timesteps + 4; //NOTE have to add for initial value. Also, must be aligned, so we can't only add one.
-	if(DataLen % 4) DataLen += (4 - DataLen % 4);    //NOTE! needed for correct simd alignment!
+	
+	#define PADDING 8   //8 probably needed for avx 512??
+	
+	u64 DataLen = Timesteps + PADDING; //NOTE have to add for initial value. Also, must be aligned, so we can't only add one.
+	if(DataLen % PADDING) DataLen += (PADDING - DataLen % PADDING);    //NOTE! needed for correct simd alignment!
 	
 	double *InputData = AllocClearedArray(double, DataLen*3);
 	
@@ -51,9 +54,9 @@ int main()
 	
 	double *SnowResults = AllocClearedArray(double, DataLen*3);
 	
-	double *SnowDepth       = SnowResults;
-	double *Infiltration    = SnowResults + DataLen;
-	double *InfiltrationEx  = SnowResults + 2*DataLen;
+	double *SnowDepth       = SnowResults                 + PADDING;
+	double *Infiltration    = SnowResults + DataLen       + PADDING;
+	double *InfiltrationEx  = SnowResults + 2*DataLen     + PADDING;
 
 	
 	double Parameters[11];
@@ -71,10 +74,10 @@ int main()
 	Parameters[9] = GetParameterDouble(DataSet, "Reach length", {Reach}) * 0.5;
 	Parameters[10] = GetParameterDouble(DataSet, "Reach slope", {Reach});
 	
-	u64 LoopSteps = DataLen / 4 - 1;
-	
-	
+	# if 1
 	{
+		int LoopSteps = (int)DataLen / 4 - PADDING / 4;
+		
 		__m256d Zero = _mm256_set1_pd(0.0);
 		__m256d One  = _mm256_set1_pd(1.0);
 		__m256d DDFMelt = _mm256_set1_pd(Parameters[0]);
@@ -82,7 +85,7 @@ int main()
 		__m256d OneMinusPquick = _mm256_set1_pd(1.0 - Parameters[1]);
 	
 		u64 Begin = __rdtsc();
-		for(u64 Step = 0; Step < LoopSteps; ++Step)
+		for(int Step = 0; Step < LoopSteps; ++Step)
 		{
 			__m256d AirT        = _mm256_load_pd(AirTemperature + 4*Step);
 			__m256d Precip      = _mm256_load_pd(Precipitation  + 4*Step);
@@ -104,11 +107,11 @@ int main()
 			
 			double Melt[4];
 			//Very unfortunate this can't be simdized:
-			for(u64 Ts = 0; Ts < 4; ++Ts)
+			for(int Ts = 0; Ts < 4; ++Ts)
 			{
-				double PrevSnowDepth = SnowDepth[4*(Step+1) + Ts - 1];
+				double PrevSnowDepth = SnowDepth[4*Step + Ts - 1];
 				Melt[Ts] = PotentialMelt[Ts] > PrevSnowDepth ? PrevSnowDepth : PotentialMelt[Ts];
-				SnowDepth[4*(Step+1) + Ts] = PrevSnowDepth + PrecipAsSnow[Ts] - Melt[Ts];
+				SnowDepth[4*Step + Ts] = PrevSnowDepth + PrecipAsSnow[Ts] - Melt[Ts];
 			}
 			
 			__m256d Meltd = _mm256_load_pd(Melt);
@@ -116,20 +119,150 @@ int main()
 			__m256d Inf = _mm256_mul_pd(HydrolInputSoil, OneMinusPquick);
 			__m256d InfEx = _mm256_mul_pd(HydrolInputSoil, Pquick);
 			
-			_mm256_store_pd(Infiltration + 4*(Step+1), Inf);
-			_mm256_store_pd(InfiltrationEx + 4*(Step+1), InfEx);
+			_mm256_store_pd(Infiltration + 4*Step, Inf);
+			_mm256_store_pd(InfiltrationEx + 4*Step, InfEx);
 		}
 		u64 End = __rdtsc();
-		std::cout << "Cycles when using SIMD: " << (End - Begin) << std::endl;
+		std::cout << "Cycles when using SIMD (AVX): " << (End - Begin) << std::endl;
 	}
+	#endif
 	
+	#if 0   //Not supported on my cpu :(
+	{
+		int LoopSteps = (int)DataLen / 8 - 1;
+		
+		__m512d Zero = _mm512_set1_pd(0.0);
+		__m512d One  = _mm512_set1_pd(1.0);
+		__m512d DDFMelt = _mm512_set1_pd(Parameters[0]);
+		__m512d Pquick = _mm512_set1_pd(Parameters[1]);
+		__m512d OneMinusPquick = _mm512_set1_pd(1.0 - Parameters[1]);
+	
+		u64 Begin = __rdtsc();
+		for(int Step = 0; Step < LoopSteps; ++Step)
+		{
+			
+			__m512d AirT        = _mm512_load_pd(AirTemperature + 8*Step);
+			__m512d Precip      = _mm512_load_pd(Precipitation  + 8*Step);
+			__mmask8 AirTGE0mask = _mm512_cmp_pd_mask(AirT, Zero, _CMP_GE_OQ);
+			__mmask8 AirTLT0mask = _mm512_cmp_pd_mask(AirT, Zero, _CMP_LT_OQ);
+			
+			__m512d Prain = _mm512_mask_blend_pd(AirTGE0mask, Zero, Precip);
+			__m512d Psnow = _mm512_mask_blend_pd(AirTLT0mask, Zero, Precip);
+			
+			__m512d PotMelt0 = _mm512_mul_pd(AirT, DDFMelt);
+			__m512d PotMelt  = _mm512_max_pd(PotMelt0, Zero);
+			
+			double PotentialMelt[8];
+			double PrecipAsRain[8];
+			double PrecipAsSnow[8];
+			_mm512_store_pd(PrecipAsRain, Prain);
+			_mm512_store_pd(PrecipAsSnow, Psnow);
+			_mm512_store_pd(PotentialMelt, PotMelt);
+			
+			double Melt[8];
+			//Very unfortunate this can't be simdized:
+			for(int Ts = 0; Ts < 8; ++Ts)
+			{
+				double PrevSnowDepth = SnowDepth[8*Step + Ts - 1];
+				Melt[Ts] = PotentialMelt[Ts] > PrevSnowDepth ? PrevSnowDepth : PotentialMelt[Ts];
+				SnowDepth[8*Step + Ts] = PrevSnowDepth + PrecipAsSnow[Ts] - Melt[Ts];
+			}
+			
+			__m512d Meltd = _mm512_load_pd(Melt);
+			__m512d HydrolInputSoil = _mm512_add_pd(Meltd, Prain);
+			__m512d Inf = _mm512_mul_pd(HydrolInputSoil, OneMinusPquick);
+			__m512d InfEx = _mm512_mul_pd(HydrolInputSoil, Pquick);
+			
+			_mm512_store_pd(Infiltration + 8*Step, Inf);
+			_mm512_store_pd(InfiltrationEx + 8*Step, InfEx);
+		}
+		u64 End = __rdtsc();
+		std::cout << "Cycles when using SIMD (AVX 512): " << (End - Begin) << std::endl;
+	}
+	#endif
+	
+	#if 1
+	{
+		//AVX256, but with single precision floats.
+		
+		float *InputData32 = AllocClearedArray(float, DataLen * 3);
+		for(int Idx = 0; Idx < DataLen*3; ++Idx)
+		{
+			InputData32[Idx] = (float)InputData[Idx];
+		}
+		
+		float *AirTemperature = InputData32;
+		float *Precipitation  = InputData32 +   DataLen;
+		float *PET            = InputData32 + 2*DataLen;
+		
+		float *ResultData32 = AllocClearedArray(float, DataLen * 3);
+		float *SnowDepth      = ResultData32             + PADDING;
+		float *Infiltration   = ResultData32 +   DataLen + PADDING;
+		float *InfiltrationEx = ResultData32 + 2*DataLen + PADDING;
+		
+		int LoopSteps = (int)DataLen / 8 - PADDING / 8;
+		
+		__m256 Zero = _mm256_set1_ps(0.0f);
+		__m256 One  = _mm256_set1_ps(1.0f);
+		__m256 DDFMelt = _mm256_set1_ps((float)Parameters[0]);
+		__m256 Pquick = _mm256_set1_ps((float)Parameters[1]);
+		__m256 OneMinusPquick = _mm256_set1_ps(1.0f - (float)Parameters[1]);
+	
+		u64 Begin = __rdtsc();
+		for(int Step = 0; Step < LoopSteps; ++Step)
+		{
+			__m256 AirT        = _mm256_load_ps(AirTemperature + 8*Step);
+			__m256 Precip      = _mm256_load_ps(Precipitation  + 8*Step);
+			__m256 AirTGE0mask = _mm256_cmp_ps(AirT, Zero, _CMP_GE_OQ);
+			__m256 AirTLT0mask = _mm256_cmp_ps(AirT, Zero, _CMP_LT_OQ);
+			
+			__m256 Prain = _mm256_blendv_ps(Zero, Precip, AirTGE0mask);
+			__m256 Psnow = _mm256_blendv_ps(Zero, Precip, AirTLT0mask);
+			
+			__m256 PotMelt0 = _mm256_mul_ps(AirT, DDFMelt);
+			__m256 PotMelt  = _mm256_max_ps(PotMelt0, Zero);
+			
+			float PotentialMelt[8];
+			float PrecipAsRain[8];
+			float PrecipAsSnow[8];
+			_mm256_store_ps(PrecipAsRain, Prain);
+			_mm256_store_ps(PrecipAsSnow, Psnow);
+			_mm256_store_ps(PotentialMelt, PotMelt);
+			
+			float Melt[8];
+			//Very unfortunate this can't be simdized:
+			for(int Ts = 0; Ts < 4; ++Ts)
+			{
+				float PrevSnowDepth = SnowDepth[8*Step + Ts - 1];
+				Melt[Ts] = PotentialMelt[Ts] > PrevSnowDepth ? PrevSnowDepth : PotentialMelt[Ts];
+				SnowDepth[8*Step + Ts] = PrevSnowDepth + PrecipAsSnow[Ts] - Melt[Ts];
+			}
+			
+			__m256 Meltd = _mm256_load_ps(Melt);
+			__m256 HydrolInputSoil = _mm256_add_ps(Meltd, Prain);
+			__m256 Inf = _mm256_mul_ps(HydrolInputSoil, OneMinusPquick);
+			__m256 InfEx = _mm256_mul_ps(HydrolInputSoil, Pquick);
+			
+			_mm256_store_ps(Infiltration + 8*Step, Inf);
+			_mm256_store_ps(InfiltrationEx + 8*Step, InfEx);
+		}
+		u64 End = __rdtsc();
+		std::cout << "Cycles when using SIMD (AVX), single precision: " << (End - Begin) << std::endl;
+		
+		//for(u64 Ts = 0; Ts < 200; ++Ts) std::cout << Infiltration[Ts] << " ";
+		//std::cout << std::endl;
+		//PrintResultSeries(DataSet, "Infiltration", {}, 200);
+	}
+	#endif
+	
+	#if 1
 	{
 		double DDFMelt = Parameters[0];
 		double Pquick  = Parameters[1];
 	
 		u64 Begin = __rdtsc();
 		
-		for(u64 Ts = 0; Ts < Timesteps; ++Ts)
+		for(int Ts = 0; Ts < (int)Timesteps; ++Ts)
 		{
 			double AirT = AirTemperature[Ts];
 			double Precip = Precipitation[Ts];
@@ -139,23 +272,19 @@ int main()
 			
 			double PotMelt = AirT > 0.0 ? AirT * DDFMelt : 0.0;
 			
-			double PrevSnowDepth = SnowDepth[Ts + 4 - 1];
+			double PrevSnowDepth = SnowDepth[Ts - 1];
 			double Melt = PotMelt > PrevSnowDepth ? PrevSnowDepth : PotMelt;
 			SnowDepth[Ts + 4] = PrevSnowDepth + Psnow - Melt;
 			
 			double HydrolInputSoil = Melt + Prain;
 			
-			Infiltration[Ts + 4] = HydrolInputSoil * (1.0 - Pquick);
-			InfiltrationEx[Ts + 4] = HydrolInputSoil * Pquick;
+			Infiltration[Ts] = HydrolInputSoil * (1.0 - Pquick);
+			InfiltrationEx[Ts] = HydrolInputSoil * Pquick;
 		}
 		
 		u64 End   = __rdtsc();
 		std::cout << "Cycles when not using SIMD: " << (End - Begin) << std::endl;
 	}
+	#endif
 	
-	/*
-	for(u64 Ts = 0; Ts < 200; ++Ts) std::cout << Infiltration[Ts + 4] << " ";
-	std::cout << std::endl;
-	PrintResultSeries(DataSet, "Infiltration", {}, 200);
-	*/
 }
