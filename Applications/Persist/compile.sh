ae503f7159e344e30e92aa67b2d431075b4bfd93
Maginor84 <magnus.dahler.norling@gmail.com>
2019-09-24 11:07:22 +0200

    Update linux compilation script

--- Applications/Persist/compile.sh
+++ Applications/Persist/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -fpic -m64 -std=c++11 -c -O2 persist_wrapper.cpp
-g++ -o persist.so -m64 -shared persist_wrapper.o
+g++ -fpic -m64 -std=c++11 -c -O2 persist_dll.cpp
+g++ -o persist.so -m64 -shared persist_dll.o
