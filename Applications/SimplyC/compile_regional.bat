f335c18914f75a48dede725f2ba73a305a4685a8
Maginor <magnus.dahler.norling@gmail.com>
2020-05-27 13:21:04 +0200

    more regional simplyc

---
+++ Applications/SimplyC/compile_regional.bat
@@ -0,0 +1,4 @@
+@echo off
+
+g++ -c -m64 -std=c++11 -O2 simplyc_regional_dll.cpp -fexceptions -fmax-errors=5
+g++ -o simplyc_regional.dll -static -static-libgcc -static-libstdc++ -s -shared simplyc_regional_dll.o -Wl,--subsystem,windows
---
+++ Applications/SimplyC/simplyc_regional_dll.cpp
@@ -0,0 +1,34 @@
+#define MOBIUS_TIMESTEP_VERBOSITY 0
+#define MOBIUS_TEST_FOR_NAN 0
+#define MOBIUS_EQUATION_PROFILING 0
+#define MOBIUS_PRINT_TIMING_INFO 0
+
+#include "../../mobius_dll.h"
+
+#include "../../Modules/UnitConversions.h"
+
+#include "../../Modules/SolarRadiation.h"
+#include "../../Modules/PET.h"
+//#define SIMPLYQ_GROUNDWATER    //NOTE: #define this before the inclusion of the SimplyQ.h file if you want SimplyQ to simulate groundwater
+								 //Comment out this line if you don't want groundwater           
+//#include "../../Modules/SimplyQ.h"
+#include "../../Modules/SimplyQ_Langtjern.h"
+
+
+#include "../../Modules/Alternate_versions_of_simplyC/SimplyC_DOC_creation_rate.h"
+#include "../../Modules/SimplySoilTemperature.h"
+
+#include "../../Modules/SimplyQLakeAddon.h"
+
+void
+DllBuildModel(mobius_model *Model)
+{
+	Model->Name = "SimplyC";
+	
+	AddMaxSolarRadiationModule(Model);
+	AddPriestleyTaylorPETModule2(Model);
+	AddSimplyHydrologyModule(Model);
+	AddSimplyQLakeAddon(Model);
+	AddSoilTemperatureModel(Model);
+	AddSimplyCModel(Model);
+}
