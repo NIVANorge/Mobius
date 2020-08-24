ec95dedfbcf596524bc39411990a69fbbb9b4827
Maginor <magnus.dahler.norling@gmail.com>
2020-08-24 16:11:15 +0200

    More SimplyN work

--- Modules/SimplyN.h
+++ Modules/SimplyN.h
@@ -11,6 +11,7 @@ AddSimplyNModel(mobius_model *Model)
 	
 	auto Dimensionless  = RegisterUnit(Model);
 	auto Kg             = RegisterUnit(Model, "kg");
+	auto Mm             = RegisterUnit(Model, "mm");
 	auto MgPerL         = RegisterUnit(Model, "mg/l");
 	auto KgPerKm2       = RegisterUnit(Model, "kg/km2");
 	auto KgPerDay       = RegisterUnit(Model, "kg/day");
@@ -31,6 +32,8 @@ AddSimplyNModel(mobius_model *Model)
 	auto SoilWaterDINImmobilisationRate   = RegisterParameterDouble(Model, NitrogenGlobal, "Soil water DIN immobilisation rate at 20°C", PerDay, 0.0, 0.0, 1.0, "", "imms");
 	auto SoilWaterDINImmobilisationQ10    = RegisterParameterDouble(Model, NitrogenGlobal, "Soil water DIN immobilisation response to 10°C change in temperature (Q10)", Dimensionless, 1.0, 1.0, 2.0, "", "Q10imms");
 	auto GroundwaterDINConcentration      = RegisterParameterDouble(Model, NitrogenGlobal, "Groundwater DIN concentration", MgPerL, 0.0, 0.0, 5.0, "", "DINgw");
+	auto GroundwaterDINConstant           = RegisterParameterBool(Model, NitrogenGlobal, "Constant groundwater DIN concentration", true, "Keep the concentration of DIN in the groundwater constant instead of simulating it.");
+	auto GroundwaterBufferVolume          = RegisterParameterDouble(Model, NitrogenGlobal, "Groundwater retention volume", Mm, 0.0, 0.0, 2000.0, "Additional dissolution buffer for DIN that does not affect the hydrology. Only used with non-constant gw concentration.");
 	
 	auto NitrogenLand   = RegisterParameterGroup(Model, "Nitrogen land", LandscapeUnits);
 	
@@ -45,12 +48,14 @@ AddSimplyNModel(mobius_model *Model)
 	auto LandSolver  = GetSolverHandle(Model, "SimplyQ land solver");
 	auto ReachSolver = GetSolverHandle(Model, "SimplyQ reach solver");
 
-	auto SoilWaterVolume    = GetEquationHandle(Model, "Soil water volume"); // [mm]
-	auto SoilWaterFlow      = GetEquationHandle(Model, "Soil water flow");   // [mm/day]
-	auto GroundwaterFlow    = GetEquationHandle(Model, "Groundwater flow");  // [mm/day]
+	auto QuickFlow          = GetEquationHandle(Model, "Quick flow");         // [mm/day]
+	auto SoilWaterVolume    = GetEquationHandle(Model, "Soil water volume");  // [mm]
+	auto SoilWaterFlow      = GetEquationHandle(Model, "Soil water flow");    // [mm/day]
+	auto GroundwaterFlow    = GetEquationHandle(Model, "Groundwater flow");   // [mm/day]
+	auto GroundwaterVolume  = GetEquationHandle(Model, "Groundwater volume"); // [mm]
 	auto DailyMeanReachFlow = GetEquationHandle(Model, "Reach flow (daily mean, cumecs)"); // [m3/day]
-	auto ReachFlow          = GetEquationHandle(Model, "Reach flow (end-of-day)");        // [m3/day]
-	auto ReachVolume        = GetEquationHandle(Model, "Reach volume");      // [m3]
+	auto ReachFlow          = GetEquationHandle(Model, "Reach flow (end-of-day)");         // [m3/day]
+	auto ReachVolume        = GetEquationHandle(Model, "Reach volume");       // [m3]
 	
 	auto BaseflowIndex      = GetParameterDoubleHandle(Model, "Baseflow index");
 	auto LandUseProportions = GetParameterDoubleHandle(Model, "Land use proportions");
@@ -65,6 +70,10 @@ AddSimplyNModel(mobius_model *Model)
 	auto DINUptake                 = RegisterEquation(Model, "DIN uptake limited by availability", KgPerHaPerDay, LandSolver);
 	auto SoilWaterDINImmobilisation= RegisterEquation(Model, "Soil water DIN immobilisation", KgPerKm2PerDay, LandSolver);
 	
+	auto QuickFlowDINFlux          = RegisterEquationODE(Model, "Quick flow DIN flux", KgPerKm2PerDay, LandSolver);
+	ResetEveryTimestep(Model, QuickFlowDINFlux);
+	auto TotalQuickFlowDINFluxToReach = RegisterEquationCumulative(Model, "Quick flow DIN flux summed over landscape units", QuickFlowDINFlux, LandscapeUnits, LandUseProportions);
+	
 	auto InitialSoilWaterDINMass   = RegisterEquationInitialValue(Model, "Initial soil water DIN mass", KgPerKm2);
 	auto SoilWaterDINMass          = RegisterEquationODE(Model, "Soil water DIN mass", KgPerKm2, LandSolver);
 	SetInitialValue(Model, SoilWaterDINMass, InitialSoilWaterDINMass);
@@ -74,9 +83,15 @@ AddSimplyNModel(mobius_model *Model)
 	
 	auto DailyMeanSoilWaterDINFluxToReach = RegisterEquationODE(Model, "Soil water DIN flux to reach (daily mean)", KgPerKm2PerDay, LandSolver);
 	ResetEveryTimestep(Model, DailyMeanSoilWaterDINFluxToReach);
-	
 	auto TotalSoilWaterDINFluxToReach = RegisterEquationCumulative(Model, "Soil water DIN flux to reach summed over landscape units", DailyMeanSoilWaterDINFluxToReach, LandscapeUnits, LandUseProportions);
 	
+	auto DailyMeanSoilWaterDINFluxToGroundwater = RegisterEquationODE(Model, "Soil water DIN flux to groundwater (daily mean)", KgPerKm2PerDay, LandSolver);
+	ResetEveryTimestep(Model, DailyMeanSoilWaterDINFluxToGroundwater);
+	auto TotalSoilWaterDINFluxToGroundwater = RegisterEquationCumulative(Model, "Soil water DIN flux to groundwater summed over landscape units", DailyMeanSoilWaterDINFluxToGroundwater, LandscapeUnits, LandUseProportions);
+	auto InitialGroundwaterDINMass  = RegisterEquationInitialValue(Model, "Initial groundwater DIN mass", KgPerKm2);
+	auto GroundwaterDINMass         = RegisterEquationODE(Model, "Groundwater DIN mass", KgPerKm2, ReachSolver);
+	SetInitialValue(Model, GroundwaterDINMass, InitialGroundwaterDINMass);
+	auto GroundwaterDINConc         = RegisterEquation(Model, "Groundwater DIN concentration", MgPerL, ReachSolver);
 	auto GroundwaterDINFluxToReach  = RegisterEquation(Model, "Groundwater DIN flux to reach", KgPerKm2PerDay, ReachSolver);
 	
 	auto InitialReachDINMass        = RegisterEquationInitialValue(Model, "Initial reach DIN mass", Kg);
@@ -135,12 +150,39 @@ AddSimplyNModel(mobius_model *Model)
 		return RESULT(SoilWaterDINConcentration) * RESULT(SoilWaterFlow);
 	)
 	
+	EQUATION(Model, QuickFlowDINFlux,
+		double dissolution_volume = RESULT(SoilWaterVolume) + RESULT(QuickFlow); //NOTE: We have to add the volume of the quick flow also, otherwise this is wrong for soil water volume close to 0.
+		return RESULT(QuickFlow) * SafeDivide(RESULT(SoilWaterDINMass), dissolution_volume);
+	)
+	
 	EQUATION(Model, DailyMeanSoilWaterDINFluxToReach,
 		return (1.0 - PARAMETER(BaseflowIndex))*RESULT(SoilWaterDINFlux);
 	)
 	
+	EQUATION(Model, DailyMeanSoilWaterDINFluxToGroundwater,
+		return PARAMETER(BaseflowIndex)*RESULT(SoilWaterDINFlux);
+	)
+	
+	EQUATION(Model, InitialGroundwaterDINMass,
+		double Vgw = PARAMETER(GroundwaterBufferVolume) + RESULT(GroundwaterVolume);
+		return Vgw*PARAMETER(GroundwaterDINConcentration);
+	)
+	
+	EQUATION(Model, GroundwaterDINMass,
+		return RESULT(TotalSoilWaterDINFluxToGroundwater) - RESULT(GroundwaterDINFluxToReach);
+	)
+	
 	EQUATION(Model, GroundwaterDINFluxToReach,
-		return RESULT(GroundwaterFlow)*PARAMETER(GroundwaterDINConcentration); // (mg/l)*mm == kg/km2
+		return RESULT(GroundwaterFlow)*RESULT(GroundwaterDINConc); // (mg/l)*mm == kg/km2
+	)
+	
+	EQUATION(Model, GroundwaterDINConc,
+		double Vgw = PARAMETER(GroundwaterBufferVolume) + RESULT(GroundwaterVolume);
+		double conc  = SafeDivide(RESULT(GroundwaterDINMass), Vgw);
+		double conc2 = PARAMETER(GroundwaterDINConcentration);
+		if(PARAMETER(GroundwaterDINConstant)) conc = conc2;
+		
+		return conc;
 	)
 	
 	
@@ -163,12 +205,13 @@ AddSimplyNModel(mobius_model *Model)
 	EQUATION(Model, ReachDINMass,
 		return
 		  RESULT(ReachDINInputFromUpstream)
-		+ (RESULT(TotalSoilWaterDINFluxToReach) + RESULT(GroundwaterDINFluxToReach))*PARAMETER(CatchmentArea)
+		+ (RESULT(TotalQuickFlowDINFluxToReach) + RESULT(TotalSoilWaterDINFluxToReach) + RESULT(GroundwaterDINFluxToReach))*PARAMETER(CatchmentArea)
 		- RESULT(ReachDINFlux);
 	)
 	
 	EQUATION(Model, ReachDINConcentration,
-		return 1e3 * SafeDivide(RESULT(DailyMeanReachDINFlux), 86400.0 * RESULT(DailyMeanReachFlow));
+		return 1e3 * SafeDivide(RESULT(ReachDINMass), RESULT(ReachVolume));
+		//return 1e3 * SafeDivide(RESULT(DailyMeanReachDINFlux), 86400.0 * RESULT(DailyMeanReachFlow));
 	)
 	
 	EQUATION(Model, DailyMeanReachDINFlux,
