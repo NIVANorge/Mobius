223a07d1a4345385602b7f5bedbe158c7c2d6739
Maginor <magnus.dahler.norling@gmail.com>
2020-10-30 11:05:25 +0100

    Add hbv test

--- Applications/HBV/hbv_dll.cpp
+++ Applications/HBV/hbv_dll.cpp
@@ -4,6 +4,7 @@
 #define MOBIUS_TEST_FOR_NAN 0
 #define MOBIUS_EQUATION_PROFILING 0
 #define MOBIUS_PRINT_TIMING_INFO 0
+#define MOBIUS_INDEX_BOUNDS_TESTS 0
 
 #include "../../mobius_dll.h"
 
---
+++ Applications/HBV/test.cpp
@@ -0,0 +1,31 @@
+#define MOBIUS_TIMESTEP_VERBOSITY 0
+#define MOBIUS_TEST_FOR_NAN 0
+#define MOBIUS_EQUATION_PROFILING 0
+#define MOBIUS_PRINT_TIMING_INFO 0
+#define MOBIUS_INDEX_BOUNDS_TESTS 0
+
+#include "../../mobius_dll.h"
+
+#include "../../Modules/HBV.h"
+
+
+int main()
+{
+	mobius_model *Model = BeginModelDefinition("SimplyP");
+	
+	AddHBVModel(Model);
+	
+	ReadInputDependenciesFromFile(Model, "langtjerninputs.dat");
+	
+	EndModelDefinition(Model);
+	
+	mobius_data_set *DataSet = GenerateDataSet(Model);
+
+	ReadParametersFromFile(DataSet, "langtjernparameters.dat");
+
+	ReadInputsFromFile(DataSet, "langtjerninputs.dat");
+	
+	PrintResultStructure(Model);
+	
+	RunModel(DataSet);
+}
