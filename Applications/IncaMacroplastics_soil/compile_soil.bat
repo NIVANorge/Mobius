42583a9611fe24af089c6420c59d559e836eafc3
Francois Clayer <francois.clayer@niva.no>
2022-09-08 10:41:46 +0200

    Rename compile_soil.bat to compile.bat

--- Applications/IncaMacroplastics_soil/compile_soil.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-g++ -c -m64 -std=c++11 -O2 incamacroplastics_soil_dll.cpp -fexceptions -fmax-errors=5
-g++ -o incamacroplastics_soil.dll -static -static-libgcc -static-libstdc++ -s -shared incamacroplastics_soil_dll.o -Wl,--subsystem,windows -luuid -lole32 -loleaut32
